3ed1e83fc23d336257b7c444b3b8546f3dd7cf1e
Oliver Kim <olive94s@gmail.com>
2021-09-13 21:14:58 -0700

    have a very rude way to present the framedata

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import json
+import os
+
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileform
@@ -8,21 +11,40 @@ from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
-    user = {'username': 'Oliver'}
-    posts = [
-        {
-            "author": {"username": "John"},
-            "body": "Beautiful day in Portland"
-        },
-        {
-            "author": {'username': 'Susan'},
-            "body": 'The Avengers movie was so cool!'
-        }
-    ]
+    # get all the frame data and pass it to the index
+    my_file_path = os.path.dirname(__file__)
+    frame_data_dir = os.path.join(my_file_path, 'frame_data')
+    frame_files = os.listdir(frame_data_dir)
+    # print("characters: %s" % " ".join(frame_files))
+    characters = [x.replace('.json', '') for x in frame_files]
 
-    return render_template('index.html', title="Home Page", posts=posts)
+    # for char in frame_files:
+    #     data_file = os.path.join(frame_data_dir, char)
+    #     # print("full path to file: %s" % " ".join(data_file))
+    #     with open(data_file, 'r') as f:
+    #         data = json.load(f) 
+    #     character_name = char.replace('.json', "")
+    #     entry = {
+    #         "name": character_name,
+    #         "data": data
+    #     }
+    #     characters.append(entry)
+    # character_names = [x.replace(".json", "") for x in characters]
+        
+    # user = {'username': 'Oliver'}
+    # posts = [
+    #     {
+    #         "author": {"username": "John"},
+    #         "body": "Beautiful day in Portland"
+    #     },
+    #     {
+    #         "author": {'username': 'Susan'},
+    #         "body": 'The Avengers movie was so cool!'
+    #     }
+    # ]
+
+    return render_template('index.html', title="Home Page", characters=characters)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -73,6 +95,94 @@ def user(username):
         {'author': user, 'body': 'Test post 1'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+def extract_data(char_name):
+    # get all the frame data and pass it to the index
+    my_file_path = os.path.dirname(__file__)
+    frame_data_dir = os.path.join(my_file_path, 'frame_data')
+    frame_files = os.listdir(frame_data_dir)
+    characters = [x.replace('.json', '') for x in frame_files]
+    
+    for char in frame_files:
+        data_file = os.path.join(frame_data_dir, char)
+        # print("full path to file: %s" % " ".join(data_file))
+        with open(data_file, 'r') as f:
+            data = json.load(f) 
+        character_name = char.replace('.json', "")
+        entry = {
+            "name": character_name,
+            "data": data
+        }
+        characters.append(entry)
+    char_data_file = os.path.join(frame_data_dir, "%s.json" % char_name)
+    if os.path.exists(char_data_file):
+        with open(char_data_file, 'r') as f:
+            char_data = json.load(f)
+        return char_data
+    else:
+        raise IOError("Could not find data for character: %s" % char_name)
+
+
+@app.route('/character/<char_name>')
+def character(char_name):
+    # in reality this is where the data should be gathered rather than index
+    # character = {
+    #     "data": [
+    #         {
+    #             "": "",
+    #             "input": "5P",
+    #             "damage": "26",
+    #             "guard": "All",
+    #             "startup": "6",
+    #             "active": "5",
+    #             "recovery": "9",
+    #             "onBlock": "-2",
+    #             "onHit": "+1",
+    #             "riscGain": "",
+    #             "level": "1",
+    #             "invuln": "none",
+    #             "prorate": "80%"
+    #         },
+    #         {
+    #             "": "",
+    #             "input": "5K",
+    #             "damage": "30",
+    #             "guard": "All",
+    #             "startup": "8",
+    #             "active": "5",
+    #             "recovery": "9",
+    #             "onBlock": "-2",
+    #             "onHit": "+1",
+    #             "riscGain": "",
+    #             "level": "1",
+    #             "invuln": "none",
+    #             "prorate": "90%"
+    #         },
+    #         {
+    #             "": "",
+    #             "input": "c.S",
+    #             "damage": "42",
+    #             "guard": "All",
+    #             "startup": "7",
+    #             "active": "7",
+    #             "recovery": "14",
+    #             "onBlock": "-2",
+    #             "onHit": "+1",
+    #             "riscGain": "",
+    #             "level": "4",
+    #             "invuln": "none",
+    #             "prorate": "100%"
+    #         }
+    #     ],
+    #     "name": char_name
+    # }
+
+    character = {
+        "name": char_name,
+        "data": extract_data(char_name)
+    }
+    return render_template('character.html', character=character)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
